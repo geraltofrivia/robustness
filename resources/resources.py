614920cac8b3421f7b8ce8065339a01051a88421
Gaurav Maheshwari <gmaheshwar@eis-macbook-08.local>
2019-08-05 13:31:34 +0200

    resource file for static lookups

---
+++ resources/resources.py
@@ -0,0 +1,12 @@
+# List of modal verbs (Found at https://www.englisch-hilfen.de/en/grammar/hilfsverben2.htm)
+modal_verbs = ["can", "could",
+               "may", "might",
+               "must", "ought to",
+               "shall", "should",
+               "will", "would"]
+
+modal_verbs_pair = [["can", "could"],
+                    ["may", "might"],
+                    ["shall", "should"],
+                    ["will", "would"]]
+
